08d2fff77c42bd7bb0dfc346c81d1c36d25f65c0
Renzo <renzofab2@hotmail.com>
2024-10-03 16:03:55 -0500

    feat: corrigiendo importación de curso

--- src/company/company/entities/company.entity.ts
+++ src/company/company/entities/company.entity.ts
@@ -1,12 +1,12 @@
 import { Agreement } from 'src/company/agreement/entities/agreement.entity';
 import { Banner } from 'src/company/banner/entities/banner.entity';
-import { Course } from 'src/course/course/entities/course.entity';
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { CompanyStatus } from '../enum/company.enum';
 import { Teacher } from 'src/course/teacher/entities/teacher.entity';
 import { ExternalCertificate } from 'src/certificate/external-certificate/entities/external-certificate.entity';
 import { VerificationCode } from 'src/auth/entities/verification-code.entity';
 import { Student } from 'src/auth/entities/student.entity';
+import { Course } from 'src/course/course/entities';
 
 @Entity('tbl_institucion')
 export class Company {
--- src/course/course/course.module.ts
+++ src/course/course/course.module.ts
@@ -1,11 +1,11 @@
-import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Course } from './entities/course.entity';
 import { Category } from './entities/category.entity';
 import { SaleModule } from 'src/sale/sale.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { Course } from './entities';
 
 @Module({
   controllers: [CourseController],
